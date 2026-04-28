af14687f9a56b7ebf601f684faed11a7915c5902
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-04-28 12:41:43 +0100

    criação restaurante.kt

---
+++ app/src/main/java/com/example/restauranteapp/Restaurante.kt
@@ -0,0 +1,12 @@
+package com.example.restauranteapp
+
+class Restaurante (
+    val id: Int,
+    val nome: String,
+    val avaliacao: Double,
+    val cozinha: String,
+    val preco: String,
+    val imagem: Int,
+    val temDesconto: Boolean
+)
+
