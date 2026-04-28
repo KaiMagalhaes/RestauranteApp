8a237b5adcda48e01386fcc533258f1bafeda2ff
K4ir0z <kai.magalhaes.324268777@msft.cesae.pt>
2026-04-28 16:34:01 +0100

    Fim Esqueleto Principal

--- app/src/main/java/com/example/restauranteapp/MainActivity.kt
+++ app/src/main/java/com/example/restauranteapp/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.exerciciooo1.Restaurante
 import com.example.exerciciooo1.RestauranteAdapter
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,23 +21,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // --- O CÓDIGO TEM DE ESTAR AQUI (ANTES DA PRÓXIMA CHAVETA) ---
+        val rv = findViewById<RecyclerView>(R.id.recycler_main)
 
-        val rv = findViewById<RecyclerView>(R.id.recycler_ex4)
-
-        val listaRestaurantes = listOf(
-            Restaurante(1, "Tasca do Zé", 4.5, "Portuguesa", "€", R.drawable.got, true),
-            Restaurante(2, "Pizza World", 4.2, "Italiana", "€€", R.drawable.got2, false),
-            Restaurante(3, "Sushi Bar", 4.8, "Japonesa", "€€€", R.drawable.got, false),
-            Restaurante(4, "Burger House", 3.9, "Americana", "€", R.drawable.got2, true),
-            Restaurante(5, "Cantina", 4.0, "Italiana", "€€", R.drawable.got, false),
-            Restaurante(6, "Tapas n' Go", 4.6, "Espanhola", "€€", R.drawable.got2, true),
-            Restaurante(7, "Puro Verde", 4.7, "Vegetariana", "€€", R.drawable.got, false),
-            Restaurante(8, "O Grelhado", 4.3, "Portuguesa", "€", R.drawable.got2, false)
+        val lista = listOf(
+            Restaurante(1, "Tasca do Zé", 4.5, "Portuguesa", "€", R.drawable.ic_launcher_background, true),
+            Restaurante(2, "Pizza World", 4.2, "Italiana", "€€", R.drawable.ic_launcher_background, false),
+            Restaurante(3, "Sushi Bar", 4.8, "Japonesa", "€€€", R.drawable.ic_launcher_background, false),
+            Restaurante(4, "Burger House", 3.9, "Americana", "€", R.drawable.ic_launcher_background, true),
+            Restaurante(5, "Cantina", 4.0, "Italiana", "€€", R.drawable.ic_launcher_background, false),
+            Restaurante(6, "Tapas n' Go", 4.6, "Espanhola", "€€", R.drawable.ic_launcher_background, true),
+            Restaurante(7, "Puro Verde", 4.7, "Vegetariana", "€€", R.drawable.ic_launcher_background, false),
+            Restaurante(8, "O Grelhado", 4.3, "Portuguesa", "€", R.drawable.ic_launcher_background, false)
         )
 
         rv.layoutManager = LinearLayoutManager(this)
-        rv.adapter = RestauranteAdapter(listaRestaurantes)
-
-    } // Esta chaveta fecha o onCreate
-} // Esta chaveta fecha a classe
+        rv.adapter = RestauranteAdapter(lista)
+    }
+}
